4969beec0ba1de9c8a0cb99e78938d9c1ccc4fcb
sukso96100 <sukso96100@gmail.com>
2018-03-26 16:51:12 +0900

    Added help command

--- src/plugs.js
+++ src/plugs.js
@@ -1,4 +1,5 @@
 exports.runPlugs = (rtm, msg, channel)=>{
   require("./weather").run(rtm, msg, channel);
   require("./aqicn").run(rtm, msg, channel);
+  require("./help").run(rtm, msg, channel);
 }
